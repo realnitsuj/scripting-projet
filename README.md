e6d9d2cde9b9672016aa128969e2b7065c51162d
realnitsuj <jbossard02@gmail.com>
2024-09-12 10:57:02 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # scripting-projet
+
+- [ ] Voir pour SQL dump
+- [ ] Créer les logs à chaque exécution
+- [ ] Voir comment envoyer les mails avec mutt
+- [ ] Faire un serveur depuis lequel
+  - on peut récupérer le fichier sql
+  - on pousser l'archive une les opérations effectuées
--- archivage.sh
+++ archivage.sh
@@ -3,21 +3,28 @@
 # importer le fichier conf
 . "/home/justin/Documents/Cours/FISE2/S7/FISE-INFORX1/Scripting System/mini-projet/archive.conf"
 
+# 
 wget --no-check-certificate "$archiveURL"
 
 archive=${archiveURL##*/}
 
-# Voir pour le warning
+# TODO Voir pour le warning
 unzip "$archive"
 
-# Somme de contrôle (sqldump)
+# sqldump
 currentSql=$()
 # Stocker la somme précédente dans la conf
 
-#gzip *.sql > $(date +'%Y%d%m').tgz
-
+# Archivage
+# TODO être plus spécifique sur le nom
 tar -czf $(date +'%Y%d%m').tgz *.sql
+
+# Ménage
 rm -vf *.sql *.zip
 
 # Pousser sur le serveur, avec SSH ou SFTP par ex
 #scp fichier destination
+
+# Logs
+
+# Mail
