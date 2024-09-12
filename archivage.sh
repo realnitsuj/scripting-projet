e98987941dd7159c3ed6d987be24e643706c7446
realnitsuj <jbossard02@gmail.com>
2024-09-12 10:45:34 +0200

    update

--- archivage.sh
+++ archivage.sh
@@ -3,16 +3,21 @@
 # importer le fichier conf
 . "/home/justin/Documents/Cours/FISE2/S7/FISE-INFORX1/Scripting System/mini-projet/archive.conf"
 
-wget --no-check-certificate "$archive"
+wget --no-check-certificate "$archiveURL"
 
-unzip ${archive##*/}
+archive=${archiveURL##*/}
+
+# Voir pour le warning
+unzip "$archive"
 
 # Somme de contrôle (sqldump)
 currentSql=$()
 # Stocker la somme précédente dans la conf
 
-gzip archive > $(date).tgz
-rm -r archive
+#gzip *.sql > $(date +'%Y%d%m').tgz
+
+tar -czf $(date +'%Y%d%m').tgz *.sql
+rm -vf *.sql *.zip
 
 # Pousser sur le serveur, avec SSH ou SFTP par ex
 #scp fichier destination
