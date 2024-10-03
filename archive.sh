72be3a458fdf11a43cd9886467b4422a88bc0d00
realnitsuj <jbossard02@gmail.com>
2024-10-03 23:41:17 +0200

    Fin mail + simplifications

--- archive.sh
+++ archive.sh
@@ -3,70 +3,79 @@
 # Importer le fichier de configuration
 source "./archive.conf"
 
-
 ###############################################
 ## Fonction d'écriture de log
 # $1 : succès (0) / échec (1) de l'opération
-# $2 : checksum du fichier concerné
-# $3 : si échec, description
+# $2 : si échec, description
+#      si succès, checksum du fichier concerné
 function ecrireLog() {
     if [[ $1 -eq 0 ]]; then
 	echo "[ $(date +'%T - %d %b %Y') ] : Succès, checksum=$2" >> "$emplacementLog"
     else
-	[[ $logStdout -eq 0 ]] && echo "$3"
-	echo "[ $(date +'%T - %d %b %Y') ] : Échec, $3" >> "$emplacementLog"
+	[[ $logStdout -eq 0 ]] && echo "$2"
+	echo "[ $(date +'%T - %d %b %Y') ] : Échec, $2" >> "$emplacementLog"
     fi
 }
 
-## TODO Fonction d'envoi de mail
+## Fonction d'envoi de mail
 # $1 : succès (0) / échec (1)
 # $2 : si échec, corps du message
 function envoyerMail() {
-    if [[ ${#mailDestinataires[*]} -ne 0 && (($1 -eq 1 && $envoyerMail -ne 0) || ($1 -eq 0 && $envoyerMail -eq 2)) ]]; then
+    if [[ ${#mailDestinataires[*]} -ne 0 && (($1 -eq 1 && $envoyerMail -eq 1) || $envoyerMail -eq 2) ]]; then
 
 	if [[ $muttrcUtilisateur -eq 0 ]]; then
 	    echo "$([[ $1 -eq 0 ]] && echo L\'opération de ce jour est un succès || echo $2)" | \
 		mutt -x \
 		     -s "$([[ $1 -eq 0 ]] && echo $objSucces || echo $objEchec)" \
-		     "$([[ ($1 -eq 0 && $joindreLog -eq 2) || ($1 -eq 1 && $joindreLog -eq 1)]] && echo -a $emplacementLog --)" \
+		     $([[ $joindreLog -eq 2 || ($1 -eq 1 && $joindreLog -eq 1) ]] && echo "-a $emplacementLog --") \
 		     "$(echo ${mailDestinataires[*]})"
-	    
+
 	else
-	    echo "$([[ $1 -eq 0 ]] && echo L\'opération de ce jour est un succès || echo $2)" | \
+	    echo "$([[ $1 -eq 0 ]] && echo L\'opération d\'archivage de ce jour est un succès. || echo $2)" | \
 		mutt -nx \
 		     -e "set from = \"$mailEnvoyeur\"" \
 		     -e "set smtp_pass = \"$motDePasse\"" \
 		     -e "set smtp_url = \"smtps://$mailEnvoyeur@$serveurHote:$port\"" \
+		     -e "set send_charset = \"utf-8\"" \
 		     -s "$([[ $1 -eq 0 ]] && echo $objSucces || echo $objEchec)" \
-		     "$([[ ($1 -eq 0 && $joindreLog -eq 2) || ($1 -eq 1 && $joindreLog -eq 1)]] && echo -a $emplacementLog --)" \
+		     $([[ $joindreLog -eq 2 || ($1 -eq 1 && $joindreLog -eq 1) ]] && echo "-a $emplacementLog --") \
 		     "$(echo ${mailDestinataires[*]})"
 	fi
-	
     fi
 
-    # TODO : écrire dans les logs en cas d'échec, sans interrompre le programme (car il l'est par ailleurs dans tous les cas)
-    [[ $? -ne 0 ]] && echo "Échec lors de l'envoi du mail."
+    [[ $? -ne 0 ]] && ecrireLog 1 "erreur lors de l'envoi du mail."
+}
+
+## Fonction combo
+# $1 : succès (0) / échec (1)
+# $2 : si échec, description
+#      si succès, checksum du fichier concerné
+function combo() {
+    if [[ "$1" -eq 0 ]]; then
+        ecrireLog 0 "$2"
+        envoyerMail 0
+    else
+        ecrireLog 1 "$2"
+        envoyerMail 1 "L'opération d'archivage de ce jour a échoué pour le motif suivant : $2"
+    fi
 }
 
 ###############################################
 # Ménage sur le serveur d'archivage
 if ! ssh "$usernameSSH@$adresseArchivage" "find $pathSSH -name *.tgz -type f -ctime +$dureeConservation -exec rm '{}' \; && exit"; then
-    ecrireLog 1 "" "impossible d'accéder au serveur d'archivage renseigné via SSH, vérifier la configuration."
-    # mail
+    combo 1 "impossible d'accéder au serveur d'archivage renseigné via SSH, vérifier la configuration."
     exit 1
 fi
 
 # Vérifier si l'URL existe
 if ! wget -q "$archiveURL"; then
-    ecrireLog 1 "" "URL renseignée inaccessible, ou \`wget\` inaccessible."
-    # mail
+    combo 1 "URL renseignée inaccessible, ou \`wget\` n'est pas installé."
     exit 1
 fi
 
 # Décompresser l'archive
 if ! unzip -q ./*.zip; then
-    ecrireLog 1 "" "erreur lors de l'invocation de \`unzip\`."
-    # mail
+    combo 1 "erreur lors de l'invocation de \`unzip\`."
     exit 1
 fi
 
@@ -75,34 +84,29 @@ rm -f ./*.zip
 
 # Vérifier si il y a un seul fichier SQL
 if [[ ! $(ls ./*.sql | wc -l) -eq 1 ]]; then
-    ecrireLog 1 "" "structure d'archive à modifier, plusieurs fichiers SQL ou organisation inadéquate."
-    # mail
+    combo 1 "structure d'archive à modifier, plusieurs fichiers SQL ou organisation inadéquate."
     exit 1
 fi
 
 # Stocker la somme de contrôle du nouveau dump
 if ! currentChecksum=$(sha256sum ./*.sql | cut -d ' ' -f1); then
-    ecrireLog 1 "" "fichier SQL non trouvé, ou \`sha256sum\` inaccessible."
-    # mail
+    combo 1 "fichier SQL non trouvé, ou \`sha256sum\` inaccessible."
     exit 1
 fi
 
-
 # Si la sauvegarde de somme de contrôle n'existe pas, la créer
-[[ -f .prevChecksum ]] || touch .prevChecksum
+touch .prevChecksum
 
 # Vérifier les changements
 if [[ "$(cat .prevChecksum)" == "$currentChecksum" ]]; then
     rm -f ./*.sql
-    ecrireLog 1 "" "somme de contrôle identique à la dernière enregistrée."
-    # Mail
+    combo 1 "somme de contrôle identique à la dernière enregistrée."
     exit 1
 fi
 
 # Archiver le nouveau dump
 if ! tar -czf "$(date +'%Y%d%m')".tgz ./*.sql; then
-    ecrireLog 1 "" "erreur lors de la compression en tgz, vérifier que \`tar\` est installé et accessible."
-    # mail
+    combo 1 "erreur lors de la compression en tgz, vérifier que \`tar\` est installé et accessible."
     exit 1
 fi
 
@@ -111,8 +115,7 @@ rm -f ./*.sql
 
 # Pousser sur le serveur, avec SSH (SFTP)
 if ! scp -q ./*.tgz "$usernameSSH@$adresseArchivage:$pathSSH"; then
-    ecrireLog 1 "" "erreur lors de la connexion SSH au serveur d'archivage, vérifier les informations de connexion."
-    # mail
+    combo 1 "erreur lors de la connexion SSH au serveur d'archivage, vérifier les informations de connexion."
     exit 1
 fi
 
@@ -122,11 +125,5 @@ rm -f ./*.tgz
 # Actualiser la somme de contrôle
 echo "$currentChecksum" > .prevChecksum
 
-# Écrire le log de succès
-ecrireLog 0 "$currentChecksum"
-
-# Mail de succès
-envoyerMail 0
-
+combo 0 "$currentChecksum"
 exit 0
-
